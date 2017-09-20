664ed61fc765163c070b56e954c0298a7843f966
Angelo <aflorend@gmail.com>
2017-09-19 21:09:03 -0500

    Integrated changes from heroku deployment.

--- assets/js/portfolio-scripts.js
+++ assets/js/portfolio-scripts.js
@@ -4,3 +4,15 @@ $('.filters-button-group > .btn').click(function(){
     $(this).addClass('selected').siblings().removeClass('selected');
   }
 });
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() >= ($('.profile-bio-section').outerHeight(true) - $('.filter-wrapper').height() + 5 )) {
+    $('.filter-wrapper').addClass('fixed-filters')
+    .width($('.container').width());
+    $('.gallery-wrapper').css('margin-top', (($('.filter-wrapper').outerHeight(true) + 35 )));
+  } else {
+    $('.filter-wrapper').removeClass('fixed-filters')
+    .width('100%');
+    $('.gallery-wrapper').css('margin-top', 0);
+  }
+});
