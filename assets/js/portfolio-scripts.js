ace915229ab6c4f22e3c25dfc71bde6f406d619d
Angelo <aflorend@gmail.com>
2017-09-20 12:15:45 -0500

    Added styling an debugging portfolio

--- assets/js/portfolio-scripts.js
+++ assets/js/portfolio-scripts.js
@@ -7,16 +7,26 @@ $('.filters-button-group > .btn').click(function(){
 
 $(window).scroll(function() {
   if ($(window).scrollTop() >= ($('.profile-bio-section').outerHeight(true) - $('.filter-wrapper').height() + 5 )) {
+    $('.follow-btn').addClass('fixed-follow-btn');
+    $('.top-btn').addClass('fixed-top-btn');
+
     $('.filter-wrapper').addClass('fixed-filters')
     .width($('.container').width());
     $('.gallery-wrapper').css('margin-top', (($('.filter-wrapper').outerHeight(true) + 35 )));
   } else {
     $('.filter-wrapper').removeClass('fixed-filters')
     .width('100%');
+
     $('.gallery-wrapper').css('margin-top', 0);
+
+    $('.top-btn').removeClass('fixed-top-btn');
   }
 });
 
 $(window).resize(function() {
   $('.fixed-filters').width($('.container').width());
+});
+
+$('.top-btn').click(function() {
+  $('html,body').animate({scrollTop: 0},600);
 });
