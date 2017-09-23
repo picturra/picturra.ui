ffdf57468614b15b758995281cc29510c1a7636b
aflorend <aflorend@gmail.com>
2017-09-23 08:50:00 -0500

    Completed lightbox gallery

---
+++ assets/js/isotope-gallery.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  // Initialize Isotope
+  var $gallery = $('.gallery-wrapper').isotope({
+    itemSelector: '.gallery-item',
+    // Masonry Options
+    masonry: {
+      gutter: 36,
+      columnWidth: '.gallery-item',
+      horizontalOrder: true
+    }
+  });
+
+  // Isotope filters
+  $('.filters-button-group').on('click', 'button', function() {
+    var filterVal = $(this).attr('data-filter');
+
+    $gallery.isotope({ filter: filterVal });
+  });
+
+  // layout Isotope after each image loads
+  $gallery.imagesLoaded().progress( function() {
+    $gallery.isotope('layout');
+  });
+});
---
+++ assets/js/lightbox-scripts.js
@@ -0,0 +1,10 @@
+// Thumbnail click handler
+$('.lightbox-thumb').click(function() {
+    let $clickedThumbSrc = $(this).attr('src');
+    let $lightboxShown = $('.lightbox-shown');
+
+    if ( $clickedThumbSrc !== $lightboxShown.attr('src') ) {
+      $lightboxShown.removeClass('lightbox-shown');
+      $('.lightbox-main').find(`[src='${$clickedThumbSrc}']`).addClass('lightbox-shown');
+    };
+});
