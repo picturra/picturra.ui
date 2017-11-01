771f7e9df48677a7e8becd729ca522812160ae2a
Angelo <aflorend@gmail.com>
2017-11-01 15:25:25 -0500

    Artist account setup initial files

---
+++ assets/js/flickity-filters.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('.filters-button-group').flickity({
+    cellAlign: 'left',
+    pageDots: false,
+    arrowShape: {
+      x0: 30,
+      x1: 60, y1: 30,
+      x2: 60, y2: 15,
+      x3: 45
+    }
+  });
+});
